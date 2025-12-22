2c97271e6d32724f909a263eb538dd70466e87d6
bala-amypo <bala@amypo.in>
2025-12-22 09:01:47 +0000

    User commit

--- src/main/java/com/example/repositories/ProductRepositories.java
+++ src/main/java/com/example/repositories/ProductRepositories.java
@@ -1,9 +1,7 @@
-// ProductRepository.java
-package com.example.demo.repository;
+package com.example.demo.repositories;
 
 import com.example.demo.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product findBySku(String sku);
 }
