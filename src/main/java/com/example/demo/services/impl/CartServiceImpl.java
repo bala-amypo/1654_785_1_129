39d90e5e8f60d068e1ab6b3913400182b7f1a081
bala-amypo <bala@amypo.in>
2025-12-27 04:16:58 +0000

    User commit

---
+++ src/main/java/com/example/demo/services/impl/CartServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.Cart;
+import com.example.demo.repository.CartRepository;
+import com.example.demo.service.CartService;
+import jakarta.persistence.EntityNotFoundException;
+
+public class CartServiceImpl implements CartService {
+    private CartRepository cartRepository;
+
+    public Cart createCart(Long userId) {
+        Cart cart = new Cart();
+        cart.setUserId(userId);
+        return cartRepository.save(cart);
+    }
+
+    public Cart getActiveCartForUser(Long userId) {
+        return cartRepository.findByUserIdAndActiveTrue(userId)
+            .orElseThrow(() -> new EntityNotFoundException("Active cart not found"));
+    }
+}
