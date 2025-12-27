89b787e3e97ab2dc5cf75014f2968bec2f3db573
bala-amypo <bala@amypo.in>
2025-12-27 07:58:38 +0000

    User commit

--- src/main/java/com/example/demo/controller/DiscountController.java
+++ src/main/java/com/example/demo/controller/DiscountController.java
@@ -4,7 +4,7 @@ import com.example.demo.model.DiscountApplication;
 import com.example.demo.service.DiscountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
