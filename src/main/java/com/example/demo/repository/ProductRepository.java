18eb68a399691c236881a43b6f517f5caa30f4bb
bala-amypo <bala@amypo.in>
2025-12-27 04:09:56 +0000

    User commit

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository {
+    Optional<Product> findBySku(String sku);
+    Product save(Product product);
+    Optional<Product> findById(Long id);
 }
---
+++ src/main/java/com/example/demo/security/JwtTockenProvider.java
@@ -0,0 +1,11 @@
+package com.example.demo.security;
+
+public class JwtTokenProvider {
+    public String generateToken(String email, String role, Long userId) {
+        return "token";
+    }
+    
+    public boolean validateToken(String token) {
+        return true;
+    }
+}
