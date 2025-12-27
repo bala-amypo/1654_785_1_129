888345b27d1ede68e39b2c021a8c710bad144d7a
bala-amypo <bala@amypo.in>
2025-12-27 05:16:58 +0000

    User commit

--- src/main/java/com/example/demo/repository/CartRepository.java
+++ src/main/java/com/example/demo/repository/CartRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.model.Cart;
 import java.util.Optional;
 
-public interface CartRepository {
+public interface CartRepository extends JpaRepository{
     Optional<Cart> findByUserIdAndActiveTrue(Long userId);
     Cart save(Cart cart);
     Optional<Cart> findById(Long id);
