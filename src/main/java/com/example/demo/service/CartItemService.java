df27c1b78794a6d614b4b1723b329d1dd7097781
bala-amypo <bala@amypo.in>
2025-12-29 09:42:40 +0000

    User commit

--- src/main/java/com/example/demo/service/CartItemService.java
+++ src/main/java/com/example/demo/service/CartItemService.java
@@ -1,8 +1,8 @@
 package com.example.demo.service;
-
+import org.springframework.stereotype.Service;
 import com.example.demo.model.CartItem;
 import java.util.List;
-
+@Service
 public interface CartItemService {
     CartItem addItemToCart(CartItem item);
     List<CartItem> getItemsForCart(Long cartId);
