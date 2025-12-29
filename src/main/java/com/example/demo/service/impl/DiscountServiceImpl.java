b2571591201b992da3157b2490460f8ae9ac5632
bala-amypo <bala@amypo.in>
2025-12-29 09:54:40 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DiscountServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DiscountServiceImpl.java
@@ -2,11 +2,11 @@ package com.example.demo.service.impl;
 
 import com.example.demo.model.*;
 import com.example.demo.repository.*;
-
+import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.*;
-
+@Service
 public class DiscountServiceImpl {
 
     private final CartRepository cartRepository;
