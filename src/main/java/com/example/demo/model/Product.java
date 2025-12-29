c08eb3f2fa2d91379e938990f31d207d141bcb1f
bala-amypo <bala@amypo.in>
2025-12-29 06:21:10 +0000

    User commit

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -5,31 +5,9 @@ import java.math.BigDecimal;
 
 @Entity
 public class Product {
+    private String category;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(unique = true)
-    private String sku;
-
-    private String name;
-    private BigDecimal price;
-    private Boolean active = true;
-
-    // getters & setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getSku() { return sku; }
-    public void setSku(String sku) { this.sku = sku; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public BigDecimal getPrice() { return price; }
-    public void setPrice(BigDecimal price) { this.price = price; }
-
-    public Boolean getActive() { return active; }
-    public void setActive(Boolean active) { this.active = active; }
+    public String getCategory() {
+        return category;
+    }
 }
